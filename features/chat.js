7becb56784c9666ed167d2fccac7bd9fea0c1693
felipekarimata <79954784+felipekarimata@users.noreply.github.com>
2021-09-28 14:23:33 -0300

    teste 2 karimata

--- features/chat.js
+++ features/chat.js
@@ -7,6 +7,8 @@ const path = require('path');
 
 module.exports = function(controller) {
 
+    
+
     // make public/index.html available as localhost/index.html
     // by making the /public folder a static/public asset
     controller.publicFolder('/', path.join(__dirname,'..','public'));
