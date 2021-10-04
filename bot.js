4ef20ded7c9be5545e3eb948d609dfeb4628073d
felipekarimata <79954784+felipekarimata@users.noreply.github.com>
2021-10-04 12:31:23 -0300

    criação de interrupts

--- bot.js
+++ bot.js
@@ -2,49 +2,64 @@
 // |__) /  \  |  |__/ |  |  
 // |__) \__/  |  |  \ |  |  
 
-// This is the main file for the botDev bot.
+// This is the main file for the Raya bot.
 
 // Import Botkit's core features
 const { Botkit } = require('botkit');
+const { BotkitCMSHelper } = require('botkit-plugin-cms');
 
-// Import a platform-specific adapter for web.
+// Import a platform-specific adapter for webex.
 
-const { WebAdapter } = require('botbuilder-adapter-web');
+const { WebexAdapter } = require('botbuilder-adapter-webex');
+
+// const { MongoDbStorage } = require('botbuilder-storage-mongodb');
 
 // Load process.env values from .env file
 require('dotenv').config();
 
 let storage = null;
+// if (process.env.MONGO_URI) {
+//     storage = mongoStorage = new MongoDbStorage({
+//         url : process.env.MONGO_URI,
+//     });
+// }
 
 
-const adapter = new WebAdapter({});
+const adapter = new WebexAdapter({})    
 
 
 const controller = new Botkit({
     webhook_uri: '/api/messages',
-
     adapter: adapter,
-
     storage
 });
 
 
 // Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
-    console.log("\n========================================== \
-        \n\nSabemi Bot is Running\n")
-    console.log(String(moment()))
-    console.log("\n==========================================")
 
     // load traditional developer-created local custom feature modules
     controller.loadModules(__dirname + '/features');
 
+    controller.interrupts("Sair" || "encerrar" || "cancelar" || "interromper" || "parar" , "message", async (bot, message) => {
+        await bot.reply(message, "");
+        await bot.cancelAllDialogs();
+    });
+
     controller.on("message", async (bot,message) => {  
         
-        console.log("Início")  
-        await bot.beginDialog("intro",{
+        console.log("inicio")  
+        await bot.beginDialog("inicio",{
         });
-    }); 
+    });
+
+});
+
+
+
+controller.webserver.get('/', (req, res) => {
+
+    res.send(`This app is running Botkit ${ controller.version }.`);
 
 });
 
